9bee13a9e3cb5a87e6f5a73f8f42fdcc62ca0617
enordstrand <eiriknordstrand@gmail.com>
2021-07-08 18:29:50 +0200

    Amir's changes 8th of July since my last visit

--- PrinceBrewery/rpiToArd.py
+++ PrinceBrewery/rpiToArd.py
@@ -3,51 +3,45 @@ import time
 from time import sleep  # Import the sleep function from the time module
 
 # ser = serial.Serial('COM3')
-# serSimPython = serial.Serial('COM4')
 
-simulation = False
 
 state2_firstRun = True
 state5_firstRun_open = True
 state5_firstRun_close = True
 state9_firstRun = True
 
-simHLTtemp = 20.0
-simBoilTemp = 20.0
-simHLTliter = 0
-simMeshLiter = 0
-simBoilLiter = 0
 
-#meshTempSP = 67.0
-meshTempSP = 37.0
-circulationTempSP = 45.0
-boilTempSP = 80.0
+# meshTempSP = 67.0
+meshTempSP = 20.0
+circulationTempSP = 25.0
+# boilTempSP = 80.0
+boilTempSP = 30.0
 
 meshTimerStart = 0.0
 circulationTimerStart = 0.0
 rinseTimerStart = 0.0
 boilTimerStart = 0.0
 
-meshTime = 10
-circulationTime = 5
-rinseTime = 10
-boilTime1 = 30
-boilTime2 = 50
+meshTime = 4500
+circulationTime = 300
+rinseTime = 300
+boilTime1 = 300
+boilTime2 = 300
 
 hltTemp = 0.0
-highLevel = 0.0
+heightLevel = 0.0
 
 rinseProcessDone = False
 
 # OUTPUTS
 v1_close = 3
 v1_open = 5
-v2_1 = 7
-v2_2 = 11
-v3_open = 13
-v3_close = 15
-v4_open = 19
-v4_close = 21
+v2_circulation = 7
+v2_boil = 11
+v3_HLT = 13
+v3_mesh = 15
+# v4_open = 19
+# v4_close = 21
 v5_open = 23
 v5_close = 29
 
@@ -56,7 +50,7 @@ p2 = 33
 
 # INPUTS
 HL1 = 35
-# HL2 = 37
+# HL2 = 20
 HL3 = 8
 # LL1 = 10
 # LL2 = 12
@@ -70,7 +64,6 @@ ser = serial.Serial(
     timeout=1
 )
 
-serSimPython = ""
 
 try:
     import RPi.GPIO as GPIO
@@ -81,12 +74,12 @@ try:
 
     GPIO.setup(v1_open, GPIO.OUT)
     GPIO.setup(v1_close, GPIO.OUT)
-    GPIO.setup(v2_1, GPIO.OUT)
-    GPIO.setup(v2_2, GPIO.OUT)
-    GPIO.setup(v3_open, GPIO.OUT)
-    GPIO.setup(v3_close, GPIO.OUT)
-    GPIO.setup(v4_open, GPIO.OUT)
-    GPIO.setup(v4_open, GPIO.OUT)
+    GPIO.setup(v2_circulation, GPIO.OUT)
+    GPIO.setup(v2_boil, GPIO.OUT)
+    GPIO.setup(v3_HLT, GPIO.OUT)
+    GPIO.setup(v3_mesh, GPIO.OUT)
+#   GPIO.setup(v4_open, GPIO.OUT)
+#   GPIO.setup(v4_open, GPIO.OUT)
     GPIO.setup(v5_open, GPIO.OUT)
     GPIO.setup(v5_close, GPIO.OUT)
     GPIO.setup(p1, GPIO.OUT)
@@ -103,23 +96,23 @@ except ImportError:
 
 def openV1():
     try:
-        print("opning v1")
+        print("opening v1")
         GPIO.output(v1_open, GPIO.HIGH)
-        sleep(1)
+        sleep(0.001)
         GPIO.output(v1_close, GPIO.LOW)
-        sleep(5)
-        print("end opning v1")
+        sleep(0.001)
+        print("end opening v1")
     except:
         print("RPI GPIO1 NOT EXIST")
 
 
-def closeV1():  # vi mA starte pump1
+def closeV1():  # vi mA start pump1
     try:
         print("closing v1")
         GPIO.output(v1_open, GPIO.LOW)
-        sleep(1)
+        sleep(0.1)
         GPIO.output(v1_close, GPIO.HIGH)
-        sleep(5)
+        sleep(0.1)
         print("end closing v1")
     except:
         print("RPI GPIO2 NOT EXIST")
@@ -128,18 +121,18 @@ def closeV1():  # vi mA starte pump1
 def setV2(i):
     try:
         if i == 1:
-            print("set_v2_1")
-            GPIO.output(v2_1, GPIO.HIGH)
-            sleep(1)
-            GPIO.output(v2_2, GPIO.LOW)
-            print("end set_v2_1")
+            print("set_v2_circulation")
+            GPIO.output(v2_circulation, GPIO.HIGH)
+            sleep(0.1)
+            GPIO.output(v2_boil, GPIO.LOW)
+            print("end set_v2_circulation")
         elif i == 2:
-            print("set_v2_2")
-            GPIO.output(v2_1, GPIO.LOW)
-            sleep(1)
-            GPIO.output(v2_2, GPIO.HIGH)
-            print("end set_v2_2")
-        sleep(5)
+            print("set_v2_boil")
+            GPIO.output(v2_circulation, GPIO.LOW)
+            sleep(0.1)
+            GPIO.output(v2_boil, GPIO.HIGH)
+            print("end set_v2_boil")
+        sleep(0.2)
     except:
         print("RPI GPIO3 NOT EXIST")
 
@@ -147,10 +140,10 @@ def setV2(i):
 def openV3():
     try:
         print("open v3")
-        GPIO.output(v3_open, GPIO.LOW)
-        sleep(1)
-        GPIO.output(v3_close, GPIO.HIGH)
-        sleep(5)
+        GPIO.output(v3_HLT, GPIO.LOW)
+        sleep(0.1)
+        GPIO.output(v3_mesh, GPIO.HIGH)
+        sleep(0.2)
         print("end open v3")
     except:
         print("RPI GPIO4 NOT EXIST")
@@ -159,10 +152,10 @@ def openV3():
 def closeV3():
     try:
         print("closing v3")
-        GPIO.output(v3_open, GPIO.HIGH)
-        sleep(1)
-        GPIO.output(v3_close, GPIO.LOW)
-        sleep(5)
+        GPIO.output(v3_HLT, GPIO.HIGH)
+        sleep(0.1)
+        GPIO.output(v3_mesh, GPIO.LOW)
+        sleep(0.2)
         print("end closing v3")
     except:
         print("RPI GPIO5 NOT EXIST")
@@ -196,9 +189,9 @@ def openV5():
     try:
         print("open v5")
         GPIO.output(v5_open, GPIO.HIGH)
-        sleep(1)
+        sleep(0.1)
         GPIO.output(v5_close, GPIO.LOW)
-        sleep(5)
+        sleep(0.2)
         print("end open v5")
     except:
         print("RPI GPIO8 NOT EXIST")
@@ -208,9 +201,9 @@ def closeV5():
     try:
         print("closing v5")
         GPIO.output(v5_open, GPIO.LOW)
-        sleep(1)
+        sleep(0.1)
         GPIO.output(v5_close, GPIO.HIGH)
-        sleep(5)
+        sleep(0.2)
         print("end closing v5")
     except:
         print("RPI GPIO9 NOT EXIST")
@@ -262,7 +255,7 @@ def state1():
         stopP2()
         print("end stop p2")
     except:
-        print("RPI GPIO NOT EXISTTT")
+        print("RPI GPIO NOT EXIST")
 
     print("start serial write")
     ser.write(b'You shall Turn off H1\r\n')
@@ -294,9 +287,8 @@ def waitForResponseAndPrint(expectedMessage):
 
 def state2():
     print("Fill HLT start")
-    global simHLTliter
     global state2_firstRun
-    global highLevel
+    global heightLevel
 
     print("start serial write to get high level HLT")
     ser.write(b'You shall Give HL1\r\n')
@@ -308,23 +300,19 @@ def state2():
     if "HL1" in temp:
         data = temp
 
-    highLevel = float(data.split(b':')[1])
-    print("The number of liters in HLT is: " + str(highLevel))
-    if highLevel > 400:
+    heightLevel = float(data.split(b':')[1])
+    print("The number of liters in HLT is: " + str(heightLevel))
+    if heightLevel > 600.0:
         startP1()
-    if state2_firstRun:
+    else:
+        stopP1()
+#    if state2_firstRun:
         openV1()
-        state2_firstRun = False
+#        state2_firstRun = False
 
-    if simulation:
-        simHLTliter += 1
-        print("SimHLTliter: " + str(simHLTliter))
-        time.sleep(1)
-
-    if highLevel > 1100.0 or simHLTliter == 10:
+    if heightLevel > 620.0:
         try:
             closeV1()
-            ###         startP1()
             return 3
         except:
             print("Not rpi")
@@ -333,30 +321,14 @@ def state2():
 
 
 def state3():
-    global simHLTtemp
     global meshTempSP
     print("Initial heating for meshing " + str(meshTempSP) + " degrees")
     # ser.write(b'You shall start PID SP;HLT;' + bytes(str(meshTempSP), "utf-8") + b'\r\n')
     ser.write(b'You shall start PID SP;HLT;' + bytes(str(meshTempSP)) + b'\r\n')
-
-    # ser.write(b'test\r\n')
-    # if simulation:
-    #     print("Sim HLT temp = " + str(simHLTtemp))
-    #     # dataSim = serSimPython.readline()
-    #     dataSim = ser.readline()
-    #
-    #     print(dataSim)
-    #
-    #     dataSimSplit = dataSim.split(b';')
-    #
-    #     ser.write(b'PID FB;' + dataSimSplit[1] + b';T1:' + bytes(str(simHLTtemp)) + b'\r\n')
-    #     # serSimPython.write(b'PID FB;' + dataSimSplit[1] + b';T1:' + bytes(str(simHLTtemp), "utf-8")
-    #     simHLTtemp += 1
-    #     time.sleep(0)
     temp = ser.readline()
     print("the temp1 is:")
     print(temp)
-    if not "sent me" in temp:
+    if "sent me" in temp is False:
         data = temp
 
     temp = ser.readline()
@@ -371,7 +343,7 @@ def state3():
     x = data.split(b';')
 
     # Maybe remove minus 2
-    if float(x[2].split(b':')[1]) < meshTempSP -2:
+    if float(x[2].split(b':')[1]) < meshTempSP:
         return 3
     else:
         print("start serial write")
@@ -384,9 +356,7 @@ def state3():
 
 
 def state4():
-    global simHLTliter
-    global simMeshLiter
-    global highLevel
+    global heightLevel
 
     print("start serial write to get high level HLT")
     ser.write(b'You shall Give HL1\r\n')
@@ -398,19 +368,14 @@ def state4():
     if "HL1" in temp:
         data = temp
 
-    highLevel = float(data.split(b':')[1])
-    print("The number of liters in HLT is: " + str(highLevel))
+    heightLevel = float(data.split(b':')[1])
+    print("The number of liters in HLT is: " + str(heightLevel))
     print("Fill mesh")
     openV3()
 
-    if simulation == True:
-        simMeshLiter += 1
-        print("Mesh-liter: " + str(simMeshLiter))
-
-    if highLevel < 950.0 or simMeshLiter == 10:
-        if simulation:
-            simHLTliter = 0
+    if heightLevel < 540.0:
         closeV3()
+        stopP1()
         return 5
     else:
         return 4
@@ -418,10 +383,9 @@ def state4():
 
 def state5():
     print("Re-filling HLT")
-    global simHLTliter
     global state5_firstRun_open
     global state5_firstRun_close
-    global highLevel
+    global heightLevel
 
     print("start serial write to get high level HLT")
     ser.write(b'You shall Give HL1\r\n')
@@ -433,18 +397,17 @@ def state5():
     if "HL1" in temp:
         data = temp
 
-    highLevel = float(data.split(b':')[1])
-
-
+    heightLevel = float(data.split(b':')[1])
+    print("The number of liters in HLT is: " + str((heightLevel/7)-54.71))
+    print("Re-filling HLT")
 
     if state5_firstRun_open:
         openV1()
         state5_firstRun_open = False
-    if simulation and simHLTliter < 10:
-        simHLTliter += 1
-        print("Liters in HLT: " + str(simHLTliter))
-
-    if highLevel > 1100.0 or simHLTliter >= 10:
+    if heightLevel > 600.0:
+        if state5_firstRun_close:
+            startP1()
+    if heightLevel > 630.0:
         if state5_firstRun_close:
             closeV1()
             state5_firstRun_close = False
@@ -455,21 +418,8 @@ def state6():
     global circulationTempSP
     print("Heating HLT " + str(circulationTempSP) + " degrees")
     global hltTemp
-    #global highLevel
+#   global heightLevel
     ser.write(b'You shall start PID SP;HLT;' + bytes(str(circulationTempSP)) + b'\r\n')
-    # if simulation == True:
-    #     global simHLTtemp
-    #     print("Sim HLT temp = " + str(simHLTtemp))
-    #     dataSim = serSimPython.readline()
-    #
-    #     print(dataSim)
-    #
-    #     dataSimSplit = dataSim.split(b';')
-    #
-    #     serSimPython.write(b'PID FB;' + dataSimSplit[1] + b';T1:' + bytes(str(simHLTtemp), "utf-8") + b'\r\n')
-    #     if simHLTtemp < circulationTempSP:
-    #         simHLTtemp += 1
-
     temp = ser.readline()
     print("the temp1 is:")
     print(temp)
@@ -487,9 +437,9 @@ def state6():
 
     x = data.split(b';')
     hltTemp = float(x[2].split(b':')[1])
-    #highLevel = float(x[5].split(b':')[1])
+    #heightLevel = float(x[5].split(b':')[1])
     print("The temperature in HLT " + str(circulationTempSP) + " is: " + str(hltTemp))
-    #print("Liters in HLT: " + str(highLevel))
+    #print("Liters in HLT: " + str(heightLevel))
     return 7
 
 
@@ -519,7 +469,6 @@ def state8():
     print("Circulation")
     currentTime = time.time()
     if circulationTimerStart == 0.0:
-        startP1()
         circulationTimerStart = time.time()
         print("circulation timer started...")
         return 8
@@ -528,16 +477,17 @@ def state8():
         return 8
     elif currentTime - circulationTimerStart >= circulationTime:
         print("Reached 80 minutes circulation at 80 degrees. Continuing!")
+        startP2()
+        setV2(1)
         return 9
 
 
 def state9():
     print("Fill boil and heat")
     global rinseProcessDone
-    global simBoilLiter
     global hltTemp
     global boilTempSP
-    global highLevel
+    global heightLevel
 
     print("start serial write to get high level HLT")
     ser.write(b'You shall Give HL3\r\n')
@@ -549,38 +499,19 @@ def state9():
     if "HL3" in temp:
         data = temp
 
-    highLevel = float(data.split(b':')[1])
+    heightLevel = float(data.split(b':')[1])
 
-    if (highLevel > 1000.0 or simBoilLiter >= 10):
+    if heightLevel > 620.0:
         stopP1()  # Maybe we can remove this.
     else:
         if state9_firstRun:
             setV2(2)
             # startP1()
-
-        if simulation:
-            simBoilLiter += 1
-            print("Boil has " + str(simBoilLiter) + " Liters ")
-
-    if highLevel > 600 or simBoilLiter > 2:
-
-        # WE NEED TO CALIBRATE OR FIX BOIL SENSOR SO THAT IT IS CORRECT ON 100 DEGREES
+    if heightLevel > 600.0:
+#   WE NEED TO CALIBRATE OR FIX BOIL SENSOR SO THAT IT IS CORRECT ON 100 DEGREES
         ser.write(b'You shall start PID SP;Boil;80\r\n')
         waitForResponseAndPrint("PID SP;Boil")
         temp = waitForResponseAndPrint("PID FB")
-
-        # if simulation:
-        #     global simBoilTemp
-        #     print("Sim boil temp = " + str(simBoilTemp))
-        #     dataSim = serSimPython.readline()
-        #
-        #     print(dataSim)
-        #
-        #     dataSimSplit = dataSim.split(b';')
-        #
-        #     serSimPython.write(b'PID FB;' + dataSimSplit[1] + b';T1:' + bytes(str(simBoilTemp), "utf-8") + b'\r\n')
-        #     if simBoilTemp < 100:
-        #         simBoilTemp += 1
 
         # temp3 = ser.readline()
         # print("the temp3 is:")
@@ -605,7 +536,7 @@ def state9():
         print("The temperature in Boil 100 is: " + str(boilTemp))
 
         if boilTemp >= boilTempSP:
-            print("The vurth is now boiling. Remember to add 60' hops!")
+            print("The wort is now boiling. Remember to add 60' hops!")
             if rinseProcessDone:
                 return 11
 
@@ -621,10 +552,9 @@ def state9():
 
 def state10():
     print("Rinse")
-    global simMeshLiter
     global rinseTimerStart  # Rinse timer can later be changed to Flow Controller
     global rinseProcessDone
-    global highLevel
+    global heightLevel
 
     print("start serial write to get high level HLT")
     ser.write(b'You shall Give HL1\r\n')
@@ -636,21 +566,15 @@ def state10():
     if "HL1" in temp:
         data = temp
 
-    highLevel = float(data.split(b':')[1])
+    heightLevel = float(data.split(b':')[1])
     startP1()
 
-    if simulation:
-
-        if simMeshLiter > 1:
-            simMeshLiter -= 1
-            print("Remaining simMeshLiter: " + str(simMeshLiter))
-
-    if (highLevel < 800 or simMeshLiter <= 1) and rinseTimerStart == 0.0:
+    if heightLevel < 800.0:
         openV3()
         startP2()
         rinseTimerStart = time.time()
         print("The rinse timer has started")
-    current_time = time.time()
+    current_time = time.time()     
     if rinseTimerStart > 0.0 and rinseProcessDone == False:
         print("The rinse process has last for: " + str(current_time - rinseTimerStart) + " seconds.")
     if rinseTimerStart > 0.0 and (current_time - rinseTimerStart > rinseTime):
